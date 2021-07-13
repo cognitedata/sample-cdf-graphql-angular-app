19fb95b0d08367a33a0f63d466252bd04b0df319
David Liu <david.liu@cognite.com>
2021-07-13 14:44:43 +0200

    fix process

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -69,3 +69,5 @@ Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
 */
+import * as process from 'process';
+window['process'] = process;
